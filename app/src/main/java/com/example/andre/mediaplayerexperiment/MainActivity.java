16ecbd9ba225015673ee22219871ef4056e4c41e
andreluiskg <andreluiskg@hotmail.com>
2017-07-11 18:06:11 -0300

    Initial commit

---
+++ app/src/main/java/com/example/andre/mediaplayerexperiment/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.andre.mediaplayerexperiment;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mMediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMediaPlayer = MediaPlayer.create(this,R.raw.musica_1);
+    }
+
+    public void playMusic(View view) {
+        mMediaPlayer.start();
+    }
+
+    public void pauseMusic(View view) {
+        mMediaPlayer.pause();
+    }
+}
