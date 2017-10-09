b0c7ba26c7802765d1b1740debb643df8501e5b1
André Luís <andreluiskg@homail.com>
2017-10-09 18:55:30 -0300

    .

--- app/src/main/java/com/example/andre/mediaplayerexperiment/MainActivity.java
+++ app/src/main/java/com/example/andre/mediaplayerexperiment/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.andre.mediaplayerexperiment;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,10 +9,14 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer=MediaPlayer.create(this,R.raw.musica_1);
 
         Button playButton = (Button) findViewById(R.id.play_button);
         Button pauseButton = (Button) findViewById(R.id.pause_button);
@@ -19,14 +24,20 @@ public class MainActivity extends AppCompatActivity {
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener(){
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        Toast.makeText(MainActivity.this,"Acabou o player",Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
         pauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Pause", Toast.LENGTH_SHORT).show();
+                mediaPlayer.pause();
             }
         });
     }
